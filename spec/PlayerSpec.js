482cd62d3d80629bef3e1912092710e5cbaea3aa
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-09 12:04:32 +0000

    Player has a type X/O

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,6 +1,12 @@
 describe('player', function(){
+
+  const player1 = new Player('Player1');
+
   it('should have a name', function(){
-    const player1 = new Player('X');
-    expect(player1.name).toEqual('X');
+    expect(player1.name).toEqual('Player1');
+  });
+  it('should have a type', function(){
+    player1.setXO('X')
+    expect(player1.type).toEqual('X');
   });
 });
--- src/Player.js
+++ src/Player.js
@@ -2,4 +2,8 @@ class Player {
   constructor(name) {
     this.name = name;
   }
+
+  setXO(type) {
+    this.type = type;
+  }
 }
